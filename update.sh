1a5bb3f6f2fabea53b1bc76e9b3656b18fb69373
Sky Moore <mskymoore@gmail.com>
2022-07-07 02:40:28 -0500

    delete update script add a couple todos

--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl delete -f nextcloud/0500_nextcloud_deployment.yml
-docker build -t skymoore/nextcloud-nginx:latest .
-docker push skymoore/nextcloud-nginx:latest
-kubectl apply -f nextcloud/0500_nextcloud_deployment.yml
